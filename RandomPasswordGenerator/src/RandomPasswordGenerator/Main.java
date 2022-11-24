cde2aecc64de169a56001d796fb805a4de8f67bd
dhavalrox21 <72266300+dhavalrox21@users.noreply.github.com>
2022-11-23 18:20:27 -0800

    Committed the code which includes random generation of password via characters and symbols.

---
+++ RandomPasswordGenerator/src/RandomPasswordGenerator/Main.java
@@ -0,0 +1,42 @@
+package RandomPasswordGenerator;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int digit = input.nextInt();
+		
+		String lower_cases = "qwertyuiopasdfghjklzxcvbnm";
+		String upper_cases = "QWERTYUIOPASDFGHJKLZXCVBNM";
+		String symbols = "!@#$%^&*()_-+={}[]|:;<,>.?/";
+		
+		String password = "";
+		
+		for(int i=0; i< digit; i++) {
+			int rand = (int)(4 * Math.random());
+			
+			switch(rand) {
+				case 0:
+					password += String.valueOf((int)(0 * Math.random()));
+					break;
+				case 1:
+					rand = (int)(lower_cases.length() * Math.random());
+					password += String.valueOf(lower_cases.charAt(rand));
+					break;
+				case 2:
+					rand = (int)(upper_cases.length() * Math.random());
+					password += String.valueOf(upper_cases.charAt(rand));
+					break;
+				case 3:
+					rand = (int)(symbols.length() * Math.random());
+					password += String.valueOf(symbols.charAt(rand));
+					break;
+			}
+					
+		}
+		System.out.println(password);
+	}
+}
---
+++ RandomPasswordGenerator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dhavaldesai
+ *
+ */
+module RandomPasswordGenerator {
+}
